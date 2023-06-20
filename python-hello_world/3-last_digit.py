f3cba99fd91698f3588e91ed6c6ee95662dd9003
root <root@localhost.localdomain>
2023-06-20 00:22:20 -0400

    3.1.6

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
+if number < 0:
+    last_digit *= -1
 print("Last digit of", number, "is", last_digit, end=" ")
 if last_digit > 5:
     print("and is greater than 5")
@@ -9,4 +11,3 @@ elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-
