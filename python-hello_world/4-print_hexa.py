55c5b0b7e7e9de40d80e9b7ee01e71598b6c04ea
root <root@localhost.localdomain>
2023-06-20 00:16:13 -0400

    3.1.5

--- python-hello_world/4-print_hexa.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-
-for num in range(99):
-    print("{} = 0x{:x}".format(num, num))
--- python-hello_world/6-print_comb3.py
+++
@@ -1,6 +0,0 @@
-for num1 in range(10):
-    for num2 in range(num1 + 1, 10):
-        if num1 < 8:
-            print("{:d}{:d}, ".format(num1, num2), end="")
-        else:
-            print("{:d}{:d}".format(num1, num2))
