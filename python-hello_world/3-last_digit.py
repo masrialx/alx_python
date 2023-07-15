f7b188e87246d3dbc750a48bb11873f2e6a55f1c
Masri Alemu <masrialemu@gmail.com>
2023-07-15 23:24:51 +0300

    commit 4.2

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,7 +4,7 @@ import random
 
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+last_digit = number % 10 if number >= 0 else -number % 10
 
 print("Last digit of", number, "is", last_digit, end=" ")
 
