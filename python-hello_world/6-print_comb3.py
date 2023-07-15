c66bd8d77fe6a3b05841a45be98284c8908e53a1
Masri Alemu <masrialemu@gmail.com>
2023-07-15 23:30:27 +0300

    commit 7.0

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
+        if num1 < 8:
+            print("{:d}{:d}, ".format(num1, num2), end="")
+        else:
+            print("{:d}{:d}".format(num1, num2))
