ded7dd90608e8faf7a75f5b13e08853591e3887b
Masri Alemu <masrialemu@gmail.com>
2023-07-20 22:53:35 +0300

    commit 3.0

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def reverse_string(string):
     reversed_string = ""
     for char in string:
         reversed_string = char + reversed_string
     return reversed_string
-input_string = "Hello, World!"
-reversed_string = reverse_string(input_string)
-print(reversed_string)
+print(reverse_string("Hello"))
+print(reverse_string(""))
+print(reverse_string("madam"))
+print(reverse_string("Hello, World!"))
 
