533021bcda4a87fb03af6e54a44a5358014bc46a
Masri Alemu <masrialemu@gmail.com>
2023-07-20 22:50:38 +0300

    commit 2.1

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
-temperature_fahrenheit = 68
-temperature_celsius = convert_to_celsius(temperature_fahrenheit)
-print(temperature_celsius)
+
+print(convert_to_celsius(100))
+print(convert_to_celsius(-40))
+print(convert_to_celsius(-459.67))
+print(convert_to_celsius(32))
