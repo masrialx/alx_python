bb9a16ba9143b329aa0ffbef3cf3f180d56da3e2
Masri Alemu <masrialemu@gmail.com>
2023-07-20 22:34:22 +0300

    commit 1.1.0

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,17 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def add(a, b):
     return a + b
 
 add = __import__('0-sum').add
 
-print(f'[Got]      {add(1, 2)}')
-print(f'[Expected] 3')
-print()
-
-print(f'[Got]      {add(98, 0)}')
-print(f'[Expected] 98')
-print()
-
-print(f'[Got]      {add(100, -2)}')
-print(f'[Expected] 98')
-
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
