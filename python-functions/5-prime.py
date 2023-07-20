5affc9e9d5814d129950a52352b41cf110e00be2
Masri Alemu <masrialemu@gmail.com>
2023-07-20 22:13:14 +0300

    commit 5.0

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -2,12 +2,16 @@
 def is_prime(number):
     if number < 2:
         return False
-    
+
     for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
-    
+
     return True
-num = 17
-is_num_prime = is_prime(num)
-print(is_num_prime)
+
+is_prime = __import__('5-prime').is_prime
+
+print(is_prime(17))
+print(is_prime(15))
+print(is_prime(-5))
+print(is_prime(0))
