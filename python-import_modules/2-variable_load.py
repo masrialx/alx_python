d55aa286937f22a5b2f407bf22c3a108f749698d
Masri Alemu <masrialemu@gmail.com>
2023-07-22 23:43:53 +0300

    commit 2.0

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-from variable_load_2 import a
 
-print(a)
+if __name__ == "__main__":
+    # Importing the variable 'a' from variable_load_2.py
+    from variable_load_2 import a
+
+    # Printing the value of the variable 'a'
+    print(a)
