a97385bc27a50255cf9dcaaab667eb4c909a29c0
Masri Alemu <masrialemu@gmail.com>
2023-07-22 23:51:05 +0300

    commit 3.1

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
     try:
         result = a / b
@@ -8,3 +7,4 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
         return result
+
