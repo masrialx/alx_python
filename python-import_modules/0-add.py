92e736885463471f1142d83eb8aa010371853860
Masri Alemu <masrialemu@gmail.com>
2023-07-22 22:53:15 +0300

    commit 0.9

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+import add_0
+
 a = 1
 b = 2
 
-from add_0 import add
-
-result = add(a, b)
+result = add_0.add(a, b)
 
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, result))
