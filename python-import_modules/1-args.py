a05658d55da8dc8134b4adb32fa93411eac32395
Masri Alemu <masrialemu@gmail.com>
2023-07-22 23:01:54 +0300

    commit 1.0

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python3
 import sys
 
+def main():
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
 
-num_args = len(sys.argv) - 1
+    if num_args > 0:
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
 
-
-if num_args == 0:
-    print("0 arguments.")
-else:
-    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
-    for i, arg in enumerate(sys.argv[1:], 1):
-        print(f"{i}: {arg}")
-
+if __name__ == "__main__":
+    main()
