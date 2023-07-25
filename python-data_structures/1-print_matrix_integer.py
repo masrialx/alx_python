930c0683baafffc8b75881300cd41c007d8ab61c
Masri Alemu <masrialemu@gmail.com>
2023-07-25 20:17:15 +0300

    commit 1.5

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
@@ -7,12 +6,3 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(row[i]))
             else:
                 print("{:d}".format(row[i]), end=" ")
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
