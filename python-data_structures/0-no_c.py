c3a6cab3250301955ec3aaba54682407007d741d
Masri Alemu <masrialemu@gmail.com>
2023-07-25 19:13:02 +0300

    commit 0.1

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     result = ""
-    i = 0
-    while i < len(my_string):
-        if my_string[i].lower() == 'c':
-            i += 1
-        else:
-            result += my_string[i]
-            i += 1
+    for char in my_string:
+        if char.lower() != 'c':
+            result += char
     return result
 
 # Test cases
-print(no_c("Holberton School"))  # Output: "Holberton School"
+print(no_c("Holberton School"))  # Output: "Holberton Shool"
 print(no_c("Chicago"))           # Output: "hiago"
 print(no_c("C is fun!"))         # Output: " is fun!"
-       
+
+word = "School"
+new_word = no_c(word)
+
+print(new_word)  # Output: "Shool"
+print(word)      # Output: "School"
