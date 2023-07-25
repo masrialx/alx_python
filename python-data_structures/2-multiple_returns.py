c2cd4d6340eb568878d68e52f7cbbca7356fce71
Masri Alemu <masrialemu@gmail.com>
2023-07-25 19:26:47 +0300

    commit 1.0

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-def multiple_returns(sentence):
-    if not sentence:
-        return 0, None
-    else:
-        return len(sentence), sentence[0]
 
-# Test case
-sentence = "At Holberton school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
