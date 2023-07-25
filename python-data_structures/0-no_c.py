33da23769961da6d6d2fd918dc6b1e933e38eb4e
Masri Alemu <masrialemu@gmail.com>
2023-07-25 19:19:31 +0300

    commit 0.2

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python3
-def no_c(my_string):
-    result = ""
-    for char in my_string:
-        if char.lower() != 'c':
-            result += char
-    return result
-
-# Test cases
-print(no_c("Holberton School"))  # Output: "Holberton Shool"
-print(no_c("Chicago"))           # Output: "hiago"
-print(no_c("C is fun!"))         # Output: " is fun!"
+#!/usr/bin/python3
+no_c = __import__('0-no_c').no_c
 
 word = "School"
 new_word = no_c(word)
 
-print(new_word)  # Output: "Shool"
-print(word)      # Output: "School"
+print(new_word)
+print(word)
+
+# Update the original 'word' variable with the new_word
+word = new_word
+
+print(word)
