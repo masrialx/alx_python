658a2e1614c88d360e29c1ec2dd4856dab8f33f5
Masri Alemu <masrialemu@gmail.com>
2023-07-25 19:36:28 +0300

    commit 2.2

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -3,10 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0] if length > 0 else None
     return (length, first)
-sentence = "At Holberton school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
-
 sentence = "Holberton"
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))
