efa7c9312a6ee51e137e6a8053f046b48b5d3e43
Masri Alemu <masrialemu@gmail.com>
2023-07-25 22:26:53 +0300

    commit 5.0

---
+++ python-functions/5-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+is_prime = __import__('5-prime').is_prime
+
+print(is_prime(17))
+print(is_prime(15))
+print(is_prime(-5))
+print(is_prime(0))
--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -8,10 +8,3 @@ def is_prime(number):
             return False
 
     return True
-
-is_prime = __import__('5-prime').is_prime
-
-print(is_prime(17))
-print(is_prime(15))
-print(is_prime(-5))
-print(is_prime(0))
