940442c611299ee98e963aa1c4239662ae64fc5f
Masri Alemu <masrialemu@gmail.com>
2023-07-25 20:39:48 +0300

    commit 1.2

--- python-functions/1-main.py
+++ python-functions/1-main.py
@@ -2,7 +2,7 @@
 pow = __import__('1-power').pow
 
 print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+print(pow(-2, 2))
+print(pow(10, -2))
+print(pow(-98, -10))
+
