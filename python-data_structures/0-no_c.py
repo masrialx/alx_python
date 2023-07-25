6400acfe49bb621fa98a6bcfd8cbfc642b10368e
Masri Alemu <masrialemu@gmail.com>
2023-07-25 20:06:44 +0300

    commit 0.5

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -5,6 +5,3 @@ def no_c(my_string):
         if char != 'c' and char != 'C':
             result += char
     return result
-print(no_c("Holberton School"))  # Output: "Holberton Shool"
-print(no_c("Chicago"))          # Output: "hiago"
-print(no_c("C is fun!"))        # Output: " is fun!"
