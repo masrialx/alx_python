51f49d24275d5dc36c6c371af27dea22a89f77e3
Masri Alemu <masrialemu@gmail.com>
2023-07-25 21:58:43 +0300

    commit 1.5

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
     result = 1
-    for _ in range(b):
-        result *= a
+
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result /= a
+
     return result
