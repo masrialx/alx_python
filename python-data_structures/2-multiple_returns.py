5b7f6b37a24f4fe5b4a1828ae263c015b89948cf
Masri Alemu <masrialemu@gmail.com>
2023-07-25 19:34:51 +0300

    commit 2.1

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -7,3 +7,6 @@ sentence = "At Holberton school, I learnt C!"
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))
 
+sentence = "Holberton"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
