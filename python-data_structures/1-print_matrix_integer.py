ffd5b2614a13d5964dde9c29ee7fc46a2b069ec8
Masri Alemu <masrialemu@gmail.com>
2023-07-25 23:04:20 +0300

    commit 2.3

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
