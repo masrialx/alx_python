66d5720f6d543bb646c65b7c5e7e200969e29b8c
Masri Alemu <masrialemu@gmail.com>
2023-07-25 22:07:32 +0300

    commit 2.0

---
+++ python-functions/2-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+convert_to_celsius = __import__('2-temperature').convert_to_celsius
+
+print(convert_to_celsius(100))
+print(convert_to_celsius(-40))
+print(convert_to_celsius(-459.67))
+print(convert_to_celsius(32))
--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -3,7 +3,4 @@ def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
 
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
+
