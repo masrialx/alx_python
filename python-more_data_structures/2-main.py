d4aa35baaf299184f188d4a75b786bd365a65e95
Masri Alemu <masrialemu@gmail.com>
2023-07-26 14:53:19 +0300

    v1.2

---
+++ python-more_data_structures/2-main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+update_dictionary = __import__('2-update_dictionary').update_dictionary
+
+def print_sorted_dictionary(my_dict):
+    """ Print sorted dictionary """
+    keys = sorted(my_dict.keys())
+    for k in keys:
+        print("{}: {}".format(k, my_dict[k]))
+
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+print("--")
+
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
