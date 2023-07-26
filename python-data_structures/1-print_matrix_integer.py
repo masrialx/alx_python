c4bd8e1b7eceb9ce2af08c4971048d6ab207f85f
Masri Alemu <masrialemu@gmail.com>
2023-07-26 14:27:47 +0300

    commit 2.2

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            if i == len(row) - 1:
-                print("{:d}".format(row[i]))
-            else:
-                print("{:d}".format(row[i]), end=" ")
+        if not row:
+            print()  
+        else:
+            for i in range(len(row)):
+                if i == len(row) - 1:
+                    print("{:d}".format(row[i]))
+                else:
+                    print("{:d}".format(row[i]), end=" ")
