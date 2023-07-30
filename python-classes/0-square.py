6899391b1bf24fb29b6e20a0e98d55acc450de93
Masri Alemu <masrialemu@gmail.com>
2023-07-30 14:19:57 +0300

    v1.3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,16 @@
 class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
-
-
-if __name__ == "__main__":
-    my_square = Square(3)
-    print(type(my_square))
-    print(my_square.__dict__)
-
-    try:
-        print(my_square.size)
-    except AttributeError as e:
-        print(e)
-
-    try:
-        print(my_square.__size)
-    except AttributeError as e:
-        print(e)
