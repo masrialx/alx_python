e3db644e86a52c41d5d05c4c3aac5a0c4d899cae
Masri Alemu <masrialemu@gmail.com>
2023-07-30 15:53:28 +0300

    v7.2

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,15 +1,42 @@
-#!/usr/bin/python3
 """
 7-rectangle.py
-Module containing the Rectangle class.
+Module containing the Rectangle class that inherits from BaseGeometry.
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """
+    BaseGeometry class.
+    """
+    def area(self):
+        """
+        Raises an Exception with the message area() is not implemented.
+
+        Raises:
+            Exception: Always raised with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value if it's an integer and greater than 0.
+
+        Args:
+            name (str): The name of the variable.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
     """
-    Rectangle class inheriting from BaseGeometry.
+    Rectangle class that inherits from BaseGeometry.
 
     Attributes:
         __width (int): The width of the rectangle.
@@ -23,10 +50,6 @@ class Rectangle(BaseGeometry):
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is not positive.
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
