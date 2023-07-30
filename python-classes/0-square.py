e2626812f56820d89016a2595244479a67e8c50c
Masri Alemu <masrialemu@gmail.com>
2023-07-30 14:02:02 +0300

    v1.1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,7 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-# Test the Square class
+
 if __name__ == "__main__":
     my_square = Square(3)
     print(type(my_square))
