98883d028f7b1c92e63d5374cb1520f21777e948
Masri Alemu <masrialemu@gmail.com>
2023-07-30 14:30:28 +0300

    v1.8

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -28,18 +28,5 @@ Todo:
 
 
 class Square:
-    """
-    Represents a square.
-
-    Attributes:
-        __size (int): The size of the square.
-    """
-
     def __init__(self, size):
-        """
-        Initializes a Square instance.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
