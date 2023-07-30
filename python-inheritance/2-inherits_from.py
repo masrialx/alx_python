dfa546580ca26061817c6c4018162d62e3d7d901
Masri Alemu <masrialemu@gmail.com>
2023-07-30 15:16:22 +0300

    v2.0

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to check if an object is an instance of a class that inherited from a specified class."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class that inherited from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of a class that inherited from the specified class,
+        False otherwise.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
