57122f5931c812bc40ccf5a36b0f17eee5e2d7c2
Masri Alemu <masrialemu@gmail.com>
2023-07-30 15:26:28 +0300

    v4.0

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module containing the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A class defining the base geometry.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
