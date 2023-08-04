7dda6cf72ee65eeb7e15c2aa381cefb396309f15
Masri Alemu <masrialemu@gmail.com>
2023-08-05 01:50:06 +0300

    8.0.2

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -33,7 +33,6 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
-
 class Rectangle(BaseGeometry):
     """
     Rectangle class that inherits from BaseGeometry.
