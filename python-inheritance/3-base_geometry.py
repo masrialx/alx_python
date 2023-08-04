1b2b5099e96749f11ac600bd4ff57c6e5c85c056
Masri Alemu <masrialemu@gmail.com>
2023-08-05 00:50:30 +0300

    v3.0.3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,15 +7,6 @@ Module containing the BaseGeometry class.
 class BaseGeometry:
     """
     An empty class defining the base geometry.
-
-    This class serves as the base class for other geometry-related classes.
-    It does not have any attributes or methods defined, and it is meant
-    to be subclassed.
-
-    Attributes:
-        None
-
-    Methods:
-        None
     """
-    pass
+    def __init_subclass__(cls):
+        pass
