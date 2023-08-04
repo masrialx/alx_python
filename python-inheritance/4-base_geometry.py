2168e55393571a3d16bfa03e49c478b07578057c
Masri Alemu <masrialemu@gmail.com>
2023-08-05 01:25:54 +0300

    v4.1

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,16 +1,33 @@
-#!/usr/bin/python3
-"""
-Module containing the BaseGeometry class.
-"""
+# -*- coding: utf-8 -*-
+"""Module containing the BaseGeometry class.
 
+This module defines the BaseGeometry class, which serves as the base class for other geometrical shapes.
+
+Example:
+    To use the BaseGeometry class, simply create an instance of it.
+
+        bg = BaseGeometry()
+        print(bg)
+
+Attributes:
+    None
+
+Todo:
+    * Add additional methods and attributes to the BaseGeometry class.
+    * Implement specific geometrical shapes based on BaseGeometry.
+
+"""
 
 class BaseGeometry:
-    """
-    A class defining the base geometry.
-    """
+    """An empty class defining the base geometry."""
 
     def area(self):
-        """
-        Raises an Exception with the message area() is not implemented.
+        """Compute the area of the geometrical shape.
+
+        This method should be implemented in subclasses to calculate the area of specific geometrical shapes.
+
+        Raises:
+            Exception: Always raises an Exception with the message "area() is not implemented".
+
         """
         raise Exception("area() is not implemented")
