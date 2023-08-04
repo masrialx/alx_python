88cc670ceb923a46be7c4eb689e4b3dc32b9b41f
Masri Alemu <masrialemu@gmail.com>
2023-08-05 00:21:41 +0300

    v2.0.4

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,12 +2,9 @@
 """
 Module to check if an object is an instance of a class that inherited from a specified class.
 """
-
-
 def inherits_from(obj, a_class):
     """
     Check if the object is an instance of a class that inherited from the specified class.
-
     Args:
         obj: The object to check.
         a_class: The class to compare against.
