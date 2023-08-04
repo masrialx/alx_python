9b32764b1ea912361b4e41a8853d73b735d94485
Masri Alemu <masrialemu@gmail.com>
2023-08-05 01:28:18 +0300

    v5.0.1

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,31 +1,48 @@
-#!/usr/bin/python3
-"""
-Module containing the BaseGeometry class.
-"""
+# -*- coding: utf-8 -*-
+"""Module containing the BaseGeometry class.
 
+This module defines the BaseGeometry class, which serves as the base class for other geometrical shapes.
+
+Example:
+    To use the BaseGeometry class, simply create an instance of it and call the `integer_validator` method.
+
+        bg = BaseGeometry()
+        bg.integer_validator("my_int", 12)
+
+Attributes:
+    None
+
+Todo:
+    * Add additional methods and attributes to the BaseGeometry class.
+    * Implement specific geometrical shapes based on BaseGeometry.
+
+"""
 
 class BaseGeometry:
-    """
-    A class defining the base geometry.
-    """
+    """An empty class defining the base geometry."""
 
     def area(self):
-        """
-        Raises an Exception with the message area() is not implemented.
+        """Compute the area of the geometrical shape.
+
+        This method should be implemented in subclasses to calculate the area of specific geometrical shapes.
+
+        Raises:
+            Exception: Always raises an Exception with the message "area() is not implemented".
+
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates the value as an integer and raises appropriate exceptions if necessary.
+        """Validate if a value is an integer and greater than 0.
 
         Args:
-            name (str): The name of the value (used in the exception message).
+            name (str): The name of the variable to be validated.
             value (int): The value to be validated.
 
         Raises:
             TypeError: If the value is not an integer.
             ValueError: If the value is less than or equal to 0.
+
         """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
