baca2acafa2830aa2f11e4998b738799424a03f9
Masri Alemu <masrialemu@gmail.com>
2023-08-05 01:08:10 +0300

    v3.4

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,12 +1,24 @@
-#!/usr/bin/python3
-"""
-Module containing the BaseGeometry class.
-"""
+# -*- coding: utf-8 -*-
+"""Module containing the BaseGeometry class.
 
+This module defines the BaseGeometry class, which is an empty class used as the base for other geometrical shapes.
+
+Example:
+    To use the BaseGeometry class, simply create an instance of it.
+
+        bg = BaseGeometry()
+        print(bg)
+
+Attributes:
+    None
+
+Todo:
+    * Add additional methods and attributes to the BaseGeometry class.
+    * Implement specific geometrical shapes based on BaseGeometry.
+
+"""
 
 class BaseGeometry:
-    """
-    An empty class defining the base geometry.
-    """
-    def __init_subclass__(cls):
-        pass
+    """An empty class defining the base geometry."""
+
+    pass
--- python-inheritance/3-main.py
+++ python-inheritance/3-main.py
@@ -4,5 +4,5 @@ BaseGeometry = __import__('3-base_geometry').BaseGeometry
 bg = BaseGeometry()
 
 print(bg)
-print(sorted(dir(bg)))  # Sort the attributes before printing
-print(sorted(dir(BaseGeometry)))  # Sort the attributes before printing
+print(dir(bg))
+print(dir(BaseGeometry))
