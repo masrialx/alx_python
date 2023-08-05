8ca29abb8e56f558e74d1d78f967033632a62d48
Masri Alemu <masrialemu@gmail.com>
2023-08-05 18:57:58 +0300

    v1.9

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,7 +8,9 @@ class Base:
     """
     Base class to manage id attribute in all future classes.
     """
+
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
         Initializes a Base instance.
