616dd2ab4d602fe69336d8d13208cd29d61793ee
Masri Alemu <masrialemu@gmail.com>
2023-08-05 18:52:09 +0300

    v1.8

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,12 +2,10 @@
 models/base.py
 Module containing the Base class that manages the id attribute for all future classes.
 """
-
 class Base:
     """
     Base class to manage id attribute in all future classes.
     """
-
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -22,4 +20,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
