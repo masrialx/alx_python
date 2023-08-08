fa749eaca0af3867fbf9225d0b126e18b6e43ee6
Masri Alemu <masrialemu@gmail.com>
2023-08-08 20:15:38 +0300

    v4.0.0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,8 @@ class Rectangle(Base):
     def area(self):
         """Calculate and return the area of the rectangle"""
         return self.__width * self.__height
+    
+    def display(self):
+        """Display the rectangle using '#' characters"""
+        for _ in range(self.__height):
+            print('#' * self.__width)
