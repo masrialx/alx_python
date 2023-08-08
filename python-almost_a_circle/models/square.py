6b83962b48740b4bed0fe9568c9bf13e3817e648
Masri Alemu <masrialemu@gmail.com>
2023-08-08 20:32:15 +0300

    v10.0

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,6 +9,17 @@ class Square(Rectangle):
         """Initialize Square instance"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for the size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string representation of the square"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
