e3c2a9afb48ff1f2d327517f96bf67468b9e04f9
Masri Alemu <masrialemu@gmail.com>
2023-08-08 20:00:44 +0300

    v1.0.5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,65 +1,13 @@
 #!/usr/bin/python3
-"""Module containing the Base class that manages the id attribute for all future classes."""
+"""Module that contains the Rectangle class"""
 
-class Base:
-    """
-    Base class to manage id attribute in all future classes.
-    """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """
-        Initializes a Base instance.
-
-        Args:
-            id (int): The id attribute for the instance.
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-    @staticmethod
-    def integer_validator(name, value):
-        """
-        Validates an integer value.
-
-        Args:
-            name (str): The name of the attribute.
-            value (int): The value to be validated.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+from models.base import Base
 
 class Rectangle(Base):
-    """
-    Rectangle class that inherits from Base.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-        __x (int): The x-coordinate of the rectangle.
-        __y (int): The y-coordinate of the rectangle.
-    """
-
+    """Rectangle class, inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Initializes a Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int): The x-coordinate of the rectangle.
-            y (int): The y-coordinate of the rectangle.
-            id (int): The id attribute for the instance.
-        """
-        super().__init__(id)
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.integer_validator("x", x)
-        self.integer_validator("y", y)
+        """Initialize Rectangle instance"""
+        super().__init__(id)  # Call Base constructor to set id
         self.width = width
         self.height = height
         self.x = x
@@ -67,12 +15,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Getter for the width attribute."""
+        """Getter method for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for the width attribute."""
+        """Setter method for width"""
         self.__width = value
 
-    # ... rest of the class
+    @property
+    def height(self):
+        """Getter method for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method for x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method for y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method for y"""
+        self.__y = value
