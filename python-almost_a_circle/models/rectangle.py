a85b918140350937e1f27909bde6b89c837b031c
Masri Alemu <masrialemu@gmail.com>
2023-08-08 19:36:47 +0300

    v1.0.3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Module containing the Rectangle class that inherits from Base."""
 from models.base import Base
 
 class Rectangle(Base):
@@ -11,51 +13,23 @@ class Rectangle(Base):
         __y (int): The y-coordinate of the rectangle.
     """
 
-    def __init__(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None, *args, **kwargs):
         """
         Initializes a Rectangle instance.
 
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the rectangle. Defaults to 0.
-            y (int, optional): The y-coordinate of the rectangle. Defaults to 0.
-            id (int, optional): The id attribute for the instance. Defaults to None.
+            x (int): The x-coordinate of the rectangle.
+            y (int): The y-coordinate of the rectangle.
+            id (int): The id attribute for the instance.
+            *args: Variable number of positional arguments.
+            **kwargs: Variable number of keyword arguments.
         """
-        # ...
+        super().__init__(id, *args, **kwargs)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-    @property
-    def width(self):
-        """Getter method for the width attribute."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter method for the width attribute."""
-        # ...
-
-    # Other methods with docstrings
-
-    def area(self):
-        """
-        Computes the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
-        return self.__width * self.__height
-
-    def display(self):
-        """
-        Prints the rectangle with the character '#'.
-        """
-        # ...
-
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle instance.
-
-        Returns:
-            str: String representation of the Rectangle instance.
-        """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+    # ... rest of the class remains the same
