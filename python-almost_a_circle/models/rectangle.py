d600edca4caaf2ae1a6b4def77d5a6abd8084e9c
Masri Alemu <masrialemu@gmail.com>
2023-08-08 19:23:16 +0300

    v1.0.1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,3 @@
-"""
-models/rectangle.py
-Module containing the Rectangle class that inherits from Base.
-"""
-
 from models.base import Base
 
 class Rectangle(Base):
@@ -10,10 +5,10 @@ class Rectangle(Base):
     Rectangle class that inherits from Base.
 
     Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-        x (int): The x-coordinate of the rectangle.
-        y (int): The y-coordinate of the rectangle.
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+        __x (int): The x-coordinate of the rectangle.
+        __y (int): The y-coordinate of the rectangle.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -23,12 +18,75 @@ class Rectangle(Base):
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-            x (int): The x-coordinate of the rectangle (default is 0).
-            y (int): The y-coordinate of the rectangle (default is 0).
-            id (int): The id attribute for the instance (default is None).
+            x (int, optional): The x-coordinate of the rectangle. Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle. Defaults to 0.
+            id (int, optional): The id attribute for the instance. Defaults to None.
         """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.integer_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.integer_validator("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.integer_validator("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.integer_validator("y", value)
+        self.__y = value
+
+    def area(self):
+        """
+        Computes the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        Prints the rectangle with the character '#'.
+        """
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle instance.
+
+        Returns:
+            str: String representation of the Rectangle instance.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
