c4917d832c15834c6f8567f10f0d2ec80dc56650
Masri Alemu <masrialemu@gmail.com>
2023-08-10 22:51:30 +0300

    v4.0

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Search API
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": q}
+
+    response = requests.post(url, data=data)
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
