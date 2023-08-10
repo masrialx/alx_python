957caca509d18097dfc058f342aacc854f9821e6
Masri Alemu <masrialemu@gmail.com>
2023-08-10 22:47:13 +0300

    v3.0

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+3. Error code #1
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
