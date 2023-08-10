e8aa041e392097a895b975cade232a367602c1b0
Masri Alemu <masrialemu@gmail.com>
2023-08-10 13:47:25 +0300

    v2.7

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,14 +1,17 @@
-"""
-Sends a POST request with an email parameter and displays the response body.
-"""
-
 import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
+
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email}
+
+    data = {
+        'email': email
+    }
 
     response = requests.post(url, data=data)
     print(response.text)
