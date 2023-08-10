dd05cda36b622b8898463a718cbbc67a635f2924
Masri Alemu <masrialemu@gmail.com>
2023-08-10 22:59:33 +0300

    v4.1

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+""" Error code #1
 """
-3. Error code #1
-"""
-
 import requests
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    response = requests.get(url)
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-    else:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP error status codes
         print(response.text)
+    except requests.exceptions.HTTPError as err:
+        print("Error code:", err.response.status_code)
