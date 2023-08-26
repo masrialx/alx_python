305831f481a76dc6fdbd978f839f62b6e772d984
Masri Alemu <masrialemu@gmail.com>
2023-08-26 11:49:26 +0300

    v1.0.1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python3
-"""
-Script to filter states starting with 'N' from a database using MySQLdb.
-"""
-
+"""Lists all states with a name starting with N from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Check for correct number of arguments
+    # Check if all arguments are provided
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
         sys.exit(1)
-    
-    # Get arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    # Connect to the database
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
-    
-    # Create a cursor
+    # Get user input from command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
     cursor = db.cursor()
-    
+
+    # Create the SQL query
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+
     # Execute the query
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' OR name LIKE 'n%' ORDER BY id ASC")
-    
+    cursor.execute(query)
+
     # Fetch and display the results
     results = cursor.fetchall()
     for row in results:
         print(row)
-    
-    # Close the cursor and database connection
+
+    # Close cursor and database connection
     cursor.close()
     db.close()
