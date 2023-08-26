967af122ac8bc0ddbbbbc6d2940d2ab5e8699200
Masri Alemu <masrialemu@gmail.com>
2023-08-26 11:53:16 +0300

    v1.3

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,23 @@
 #!/usr/bin/env python3
-"""Lists all states with a name starting with N (case-insensitive) from the database hbtn_0e_0_usa"""
-import sys
+"""Lists all states with a name starting with N from hbtn_0e_0_usa"""
+
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2] 
+    db_name = sys.argv[3]
 
-    # Get user input from command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, 
+                         user=username, passwd=password, db=db_name)
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
     cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
 
-    # Create the SQL query
-    query = "SELECT * FROM states WHERE name LIKE 'N%' COLLATE utf8_general_ci ORDER BY id ASC"
-
-    # Execute the query
-    cursor.execute(query)
-
-    # Fetch and display the results
-    results = cursor.fetchall()
-    for row in results:
+    for row in rows:
         print(row)
 
-    # Close cursor and database connection
     cursor.close()
     db.close()
