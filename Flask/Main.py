9a0b186180820cdec3822c38c97c9a08e96adec5
Masri Alemu <masrialemu@gmail.com>
2023-08-28 10:27:21 +0300

    v0.1

--- Flask/Main.py
+++ Flask/Main.py
@@ -1,9 +1,22 @@
+"""
+0. Hello Flask!
+Starts a Flask web application.
+
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+You must use the option strict_slashes=False in your route definition
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when accessing the root URL.
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
