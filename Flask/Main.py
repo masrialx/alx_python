eb5726346e01c0a8fec9a9b39d1e941adbdcb0f1
Masri Alemu <masrialemu@gmail.com>
2023-08-28 10:55:53 +0300

    v0.4

--- Flask/Main.py
+++ Flask/Main.py
@@ -1,30 +1,13 @@
-"""
-This script creates a simple Flask web application.
-
-Usage:
-    Run this script and access the web application in your browser or using curl.
-
-Routes:
-    /: Displays "Hello HBNB!"
-
-Note:
-    The web application listens on 0.0.0.0 and port 5000.
-"""
+"""Flask app that displays 'Hello HBNB!'"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """
-    This function defines the route '/' which displays "Hello HBNB!".
-    It uses the route() decorator from Flask to bind the function to the URL.
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
 
-    Returns:
-        str: The string "Hello HBNB!" which is displayed in the browser.
-    """
-    return "Hello HBNB!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    """runs the Flask app"""
+    app.run(host='0.0.0.0', port=5000)
