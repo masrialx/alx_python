51d025e23783cfc3bd376cece008ff4106a8ca89
Masri Alemu <masrialemu@gmail.com>
2023-08-28 11:00:03 +0300

    v0.6

--- Flask/Main.py
+++ Flask/Main.py
@@ -1,32 +1,26 @@
-"""
-Flask web application to display 'Hello HBNB!'.
+"""Flask web application to display 'Hello HBNB!'.
 
-The app handles requests to the route '/' to display a greeting.
+The module creates a Flask application that handles 
+requests to the route '/' to display a greeting.
 """
 
 from flask import Flask
 
 class HBNBFlaskApp(Flask):
-    """Flask application class for 'Hello HBNB!'.
+  """Flask application class to display 'Hello HBNB!'.
 
-    Handles requests to route '/' to display greeting.
-    """
-    
-    def __init__(self, name):
-        super().__init__(name)
-        
-        @self.route('/', strict_slashes=False)
-        def display_greeting():
-            """Display 'Hello HBNB!' greeting.
+  Handles requests to route '/' to display greeting.
+  """
 
-            Returns the string 'Hello HBNB!'.
-            """
-            return 'Hello HBNB!'
-            
+  def __init__(self, name):
+    super().__init__(name)
+
+    @self.route('/', strict_slashes=False)
+    def display_greeting():
+      """Display 'Hello HBNB!' greeting."""
+      return 'Hello HBNB!'
+      
 if __name__ == '__main__':
-    """Runs the Flask web application.
-
-    Creates a Flask app instance and runs it on host '0.0.0.0' port 5000.
-    """
-    app = HBNBFlaskApp(__name__)
-    app.run(host='0.0.0.0', port=5000)
+  """Run the Flask web application."""
+  app = HBNBFlaskApp(__name__)
+  app.run(host='0.0.0.0', port=5000)
