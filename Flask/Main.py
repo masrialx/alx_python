51a490c69e14a1a6463f66ebec928e9a72a2f0de
Masri Alemu <masrialemu@gmail.com>
2023-08-28 10:48:18 +0300

    v0.3

--- Flask/Main.py
+++ Flask/Main.py
@@ -1,29 +1,30 @@
 """
-0. Hello Flask!
-A simple Flask web application that displays "Hello HBNB!" at the root URL.
-
-This script starts a Flask web application that listens on 0.0.0.0 and port 5000.
-The only route defined is '/', which displays the message "Hello HBNB!".
-The option strict_slashes is set to False to allow for both '/hello' and '/hello/' routes.
+This script creates a simple Flask web application.
 
 Usage:
-    Run this script using Python to start the Flask server.
-    The application will be accessible at http://0.0.0.0:5000/.
+    Run this script and access the web application in your browser or using curl.
+
+Routes:
+    /: Displays "Hello HBNB!"
+
+Note:
+    The web application listens on 0.0.0.0 and port 5000.
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route("/", strict_slashes=False)
+def hello():
     """
-    Route handler for the root URL.
-    
-    Returns:
-        str: A plain text message "Hello HBNB!".
-    """
-    return 'Hello HBNB!'
+    This function defines the route '/' which displays "Hello HBNB!".
+    It uses the route() decorator from Flask to bind the function to the URL.
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    Returns:
+        str: The string "Hello HBNB!" which is displayed in the browser.
+    """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
