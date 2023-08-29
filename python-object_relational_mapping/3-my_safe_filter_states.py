63183479d2afdb70633607997939650624e4ca23
Masri Alemu <masrialemu@gmail.com>
2023-08-29 23:26:57 +0300

    v3.2

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
 """Safely filters states by user input"""
+
 import sys
 import MySQLdb
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: {} <mysql username> <mysql password> <database name> <state name searched>".format(sys.argv[0]))
         sys.exit(1)
+
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name_searched = sys.argv[4]
+
     db = MySQLdb.connect(
-        host="localhost",port=3306, user=mysql_username,passwd=mysql_password, db=database_name
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
+
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cursor.execute(query, (state_name_searched,))
     results = cursor.fetchall()
+
     for row in results:
         print(row)
+
     cursor.close()
     db.close()
