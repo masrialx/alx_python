99f096eac821dfefcb504388bcfb7fe6ae78df3c
Masri Alemu <masrialemu@gmail.com>
2023-08-29 23:12:19 +0300

    v2.0

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,45 @@
-#!/usr/bin/env python3
-"""Filters states by user input"""
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
+    # Check if all 4 arguments are provided
     if len(sys.argv) != 5:
-        print("Usage: {} <mysql username> <mysql password> <database name> <state name searched>".format(sys.argv[0]))
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
         sys.exit(1)
 
-    # Get user input from command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name_searched = sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor
     cursor = db.cursor()
 
-    # Create the SQL query with user input using format
+    # Execute the query to retrieve states where name matches the input
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name_searched,))
+    cursor.execute(query, (state_name,))
 
-    # Fetch and display the results
+    # Fetch all the results
     results = cursor.fetchall()
+
+    # Print the results
     for row in results:
         print(row)
 
-    # Close cursor and database connection
+    # Close the cursor and the database connection
     cursor.close()
     db.close()
