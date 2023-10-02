c00db97c6d344124bed29643d25e0f5de7c549f6
Masri Alemu <masrialemu@gmail.com>
2023-10-02 21:39:28 +0300

    v0.4

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,16 +13,15 @@ def get_employee_data(employee_id):
     todos_response = requests.get(f"{base_url}/todos?userId={employee_id}")
     todos_data = todos_response.json()
 
-    # Calculate the number of completed and total tasks
+    return employee_name, todos_data
+
+def display_employee_progress(employee_name, todos_data):
+    completed_tasks = [todo for todo in todos_data if todo['completed']]
     total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    return employee_name, completed_tasks, total_tasks, todos_data
-
-def display_employee_progress(employee_name, completed_tasks, total_tasks, completed_task_titles):
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-    for title in completed_task_titles:
-        print(f"    {title}")
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"    {task['title']}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -32,11 +31,9 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
 
     try:
-        employee_name, completed_tasks, total_tasks, todos_data = get_employee_data(employee_id)
+        employee_name, todos_data = get_employee_data(employee_id)
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         sys.exit(1)
 
-    completed_task_titles = [todo['title'] for todo in todos_data if todo['completed']]
-
-    display_employee_progress(employee_name, completed_tasks, total_tasks, completed_task_titles)
+    display_employee_progress(employee_name, todos_data)
